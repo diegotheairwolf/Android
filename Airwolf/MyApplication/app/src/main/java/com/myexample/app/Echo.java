6b9031c1572d51f74a29bab4555cbebc5d088235
diegotheairwolf <diegomoreno@gmail.com>
2014-02-15 15:41:03 -0600

    Doing tutorial

--- Airwolf/MyApplication/app/src/main/java/com/myexample/app/Echo.java
+++ Airwolf/MyApplication/app/src/main/java/com/myexample/app/Echo.java
@@ -29,6 +29,11 @@ public class Echo extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_echo);
 
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
