57941b558634c90c1a0e65e99580476a322d262d
Andoni <andoni.mendoza@gmail.com>
2014-03-03 22:48:22 -0600

    merge sms loc and ui

--- Doni/eclipse-workspace/SMS/src/selva/sms/SMSActivity.java
+++ Doni/eclipse-workspace/SMS/src/selva/sms/SMSActivity.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.gsm.SmsManager;
 import android.view.View;
@@ -111,6 +112,57 @@ public class SMSActivity extends Activity {
 		Log.i("***** address:", str);
 		return str;
 	}
+	
+	
+	
+	
+	static final int GET_ADDRESS_REQUEST = 1;  // The request code
+
+	private void pickContact() {
+	    Intent getAddress = new Intent(Intent.ACTION_PICK);
+	    getAddress.setType(null); // Show user only contacts w/ phone numbers
+	    startActivityForResult(getAddress, GET_ADDRESS_REQUEST);
+	}
+	
+	
+	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    // Check which request it is that we're responding to
+	    if (requestCode == GET_ADDRESS_REQUEST) {
+	        // Make sure the request was successful
+	        if (resultCode == RESULT_OK) {
+	        	
+	        	
+	        	
+	        	
+	            // Get the URI that points to the selected contact
+	            // Uri contactUri = data.getData(); // *** commented out
+	            // We only need the NUMBER column, because there will be only one row in the result
+	            String[] projection = {Phone.NUMBER};
+
+	            // Perform the query on the contact to get the NUMBER column
+	            // We don't need a selection or sort order (there's only one result for the given URI)
+	            // CAUTION: The query() method should be called from a separate thread to avoid blocking
+	            // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+	            // Consider using CursorLoader to perform the query.
+	            Cursor cursor = getContentResolver()
+	                    .query(contactUri, projection, null, null, null);
+	            cursor.moveToFirst();
+
+	            // Retrieve the phone number from the NUMBER column
+	            int column = cursor.getColumnIndex(Phone.NUMBER);
+	            String number = cursor.getString(column);
+
+	            // Do something with the phone number...
+	        }
+	    }
+	}
+	
+	
+	
+	
 	
 	
 	
